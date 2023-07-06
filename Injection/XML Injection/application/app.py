f954e2c51de592d84db7c9cea676b84b14420dc8
Joshua Franco <francoswim@yahoo.com>
2023-07-06 19:50:44 -0400

    XML upload feature and search directory

--- Injection/XML Injection/application/app.py
+++ Injection/XML Injection/application/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mysql.connector
 import xml.etree.ElementTree as ET
 import xml.dom.minidom
@@ -8,49 +8,88 @@ app.secret_key = 'password-test'
 
 @app.route('/')
 def index():
-    # Connect to the MySQL database
-    db = mysql.connector.connect(
-        host='db',
-        user='myuser',
-        password='mypassword',
-        database='mydatabase'
-    )
-    cursor = db.cursor()
+    return render_template("index.html")
 
-    # Fetch data from the database
-    cursor.execute("SELECT * FROM employees")
-    results = cursor.fetchall()
 
-    # Create the root element
-    root = ET.Element("employees")
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        # Render the upload form template
+        return render_template('upload.html')
+    elif request.method == 'POST':
+        # Check if an XML file was provided
+        if 'file' not in request.files:
+            return 'No file selected'
 
-    # Iterate over the results and create XML elements
-    for row in results:
-        employee = ET.SubElement(root, "employee")
-        id = ET.SubElement(employee, "id")
-        id.text = str(row[0])
-        name = ET.SubElement(employee, "name")
-        name.text = row[1]
-        email = ET.SubElement(employee, "email")
-        email.text = row[2]
-        password = ET.SubElement(employee, "password")
-        password.text = row[3]
-        phone_number = ET.SubElement(employee, "phone_number")
-        phone_number.text = row[4]
+        xml_file = request.files['file']
+        xml_data = xml_file.read()
 
-    # Generate XML string from the root element
-    xml_str = ET.tostring(root).decode()
+        # Parse the XML data
+        try:
+            root = ET.fromstring(xml_data)
+        except ET.ParseError as e:
+            return f'Error parsing XML: {str(e)}'
 
-    # Prettify the XML string
-    dom = xml.dom.minidom.parseString(xml_str)
-    pretty_xml_str = dom.toprettyxml()
+        # Connect to the MySQL database
+        db = mysql.connector.connect(
+            host='db',
+            user='myuser',
+            password='mypassword',
+            database='mydatabase'
+        )
+        cursor = db.cursor()
 
-    # Close the database connection
-    cursor.close()
-    db.close()
+        # Extract data from XML and insert into the employees table
+        for employee in root.findall('employee'):
+            name = employee.find('name').text
+            email = employee.find('email').text
+            password = employee.find('password').text
+            phone_number = employee.find('phone_number').text
 
-    # Render the XML template
-    return render_template("index.html", xml_data=pretty_xml_str)
+            # Insert data into the employees table
+            query = "INSERT INTO employees (name, email, password, phone_number) VALUES (%s, %s, %s, %s)"
+            values = (name, email, password, phone_number)
+            cursor.execute(query, values)
+            db.commit()
+
+        # Close the database connection
+        cursor.close()
+        db.close()
+
+        return 'XML data uploaded successfully'
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'GET':
+        # Render the search form template
+        return render_template('search.html')
+    elif request.method == 'POST':
+        # Retrieve the keyword from the form submission
+        keyword = request.form.get('keyword')
+
+        # Connect to the MySQL database and perform the search
+        db = mysql.connector.connect(
+            host='db',
+            user='myuser',
+            password='mypassword',
+            database='mydatabase'
+        )
+        cursor = db.cursor()
+
+        # Execute the search query
+        query = "SELECT * FROM employees WHERE email LIKE %s"
+        values = (f'%{keyword}%',)
+        cursor.execute(query, values)
+        results = cursor.fetchall()
+
+        # Close the database connection
+        cursor.close()
+        db.close()
+
+        # Render the search results template with the results and keyword
+        return render_template('search.html', results=results, keyword=keyword)
+
 
 
 if __name__ == '__main__':
--- Injection/XML Injection/db/init.sql
+++ Injection/XML Injection/db/init.sql
@@ -2,27 +2,6 @@ CREATE DATABASE IF NOT EXISTS mydatabase;
 
 USE mydatabase;
 
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255),
-    role VARCHAR(255)
-);
-
-INSERT INTO users (username, email, password, role)
-VALUES ('Admin', 'Admin@hackme.com', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 'admin');
-
-CREATE TABLE IF NOT EXISTS submissions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255),
-    message TEXT
-);
-
-INSERT INTO submissions (name, email, message)
-VALUES ('Admin', 'Admin@hackme.com', 'There is a known issue with this websites login page. You can bypass the login!');
-
 CREATE TABLE IF NOT EXISTS employees (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
